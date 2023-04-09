4838e049818dc7d49d117aa5f1b507080047d30e
chanhocode <chjo1245@gmail.com>
2023-04-10 00:08:54 +0900

    Feat: 스크롤바 커스텀 및 메시지 전송시간 포맷 변경

--- client/components/ChatList/index.tsx
+++ client/components/ChatList/index.tsx
@@ -1,19 +1,25 @@
 import { IDM } from '@typings/db';
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useCallback, useRef } from 'react';
 import { ChatZone, Section } from './styles';
 import Chat from '../Chat';
+import { Scrollbars } from 'react-custom-scrollbars';
+
 interface Props {
   chatData?: IDM[];
 }
 
+// autoHide 스크롤을 움직일때만 스크롤 표시
 const ChatList: FunctionComponent<Props> = ({ chatData }) => {
+  const scrollbarRef = useRef(null);
+  const onScroll = useCallback(() => {}, []);
+
   return (
     <ChatZone>
-      <div>
+      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
         {chatData?.map((chat) => (
           <Chat key={chat.id} data={chat} />
         ))}
-      </div>
+      </Scrollbars>
     </ChatZone>
   );
 };
