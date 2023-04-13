875a029101ab2b7b75e1128c1cfb43a33d537049
chanhocode <chjo1245@gmail.com>
2023-04-13 22:58:10 +0900

    Fix: useInput 타입 변경

--- client/hooks/useInput.ts
+++ client/hooks/useInput.ts
@@ -2,7 +2,7 @@ import React, { SetStateAction, useCallback, useState, Dispatch, ChangeEvent } f
 
 type ReturnTypes<T> = [T, (e: ChangeEvent<HTMLInputElement>) => void, Dispatch<SetStateAction<T>>];
 
-const useInput = <T extends string | number>(initialData: T): ReturnTypes<T> => {
+const useInput = <T>(initialData: T): ReturnTypes<T> => {
   const [value, setValue] = useState(initialData);
   const handler = useCallback((e) => {
     setValue(e.target.value as unknown as T);
